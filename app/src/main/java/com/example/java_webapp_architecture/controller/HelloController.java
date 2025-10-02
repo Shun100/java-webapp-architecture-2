cc1a4caa287094d11f7e82c877733e62cffc992b
Shun100 <denkidenki55@gmail.com>
2025-10-02 09:51:14 +0900

    WIP

--- app/src/main/java/com/example/java_webapp_architecture/controller/HelloController.java
+++ app/src/main/java/com/example/java_webapp_architecture/controller/HelloController.java
@@ -3,10 +3,12 @@ package com.example.java_webapp_architecture.controller;
 // import org.springframework.web.bind.annotation.Controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
-  @GetMapping("/hello")
+  @GetMapping("/")
   public String sayHello() {
     return "Hello";
   }

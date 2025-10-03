7b90d998e4de145d598da61a244bf12a0df8c845
Shun Takamura <shun@ShunnoMacBook-Air.local>
2025-10-02 17:22:23 -0700

    WIP PersonController

--- app/src/main/java/com/example/java_webapp_architecture/controller/PersonController.java
+++ app/src/main/java/com/example/java_webapp_architecture/controller/PersonController.java
@@ -2,14 +2,18 @@ package com.example.java_webapp_architecture.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.java_webapp_architecture.dto.PersonDto;
 
 @Controller
-@RequestMapping("/person")
+@RequestMapping("/spring_mvc_person")
 public class PersonController {
+  @Autowired
+  private final PersonService personService = new PersonService();
 
   /**
    * 新規登録画面を開く
@@ -17,9 +21,35 @@ public class PersonController {
    * @return String personInputPage - 新規登録画面のHTML
    */
   @GetMapping("/create")
-  public String createPerson(Model model) {
-    PersonDto person = new PersonDto(1, "Alice", 26, "female");
-    model.addAttribute("person", person);
+  public String createPerson(@RequestParam int personId, Model model) {
+    // PersonDto person = new PersonDto(1, "Alice", 26, "female");
+    // model.addAttribute("person", person);
+    Option<PersonDto> optionalPerson = personService.getById(personId);
+    optionalPerson.ifPresent(person -> model.addAttribute("person", person));
     return "personInputPage";
   }
+
+  @GetMapping("/confirm")
+  public String cofirm(Model model) {
+    return "";
+  }
+
+  @GetMapping("/edit")
+  public String edit(Model model) {
+    return "";
+  }
+
+  @GetMapping("/delete")
+  public String delete(Model model) {
+    return "";
+  }
+
+  @GetMapping("/table")
+  public String getTable(Model model) {
+    return "personTablePage";
+  }
+
+
+
+
 }
